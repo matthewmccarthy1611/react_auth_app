684b2916495b4351ab0c2f068769ecde65e4e17b
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-04 12:01:03 -0500

    Create registration form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Registration from './auth/Registration';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                
+                <Registration />
             </div>
         );
     }
---
+++ src/components/auth/Registration.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Registration extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            password_confirmation: '',
+            registrationErrors: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        axios.post('http://localhost:3001/registrations', {
+            user: {
+                email: this.state.email,
+                password: this.state.password,
+                password_confirmation: this.state.password_confirmation
+            }
+        }, {
+            withCredentials: true
+        })
+            .then(res => {
+                console.log('response',res)
+            })
+            .then(error => {
+                console.log('error', error)
+            })
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit} >
+                    <input 
+                        type='email'
+                        name='email' 
+                        placeholder='Email' 
+                        value={this.state.email} 
+                        onChange={this.handleChange}
+                        required />
+                    <input 
+                        type='password'
+                        name='password' 
+                        placeholder='Password' 
+                        value={this.state.password} 
+                        onChange={this.handleChange}
+                        required />
+                    <input 
+                        type='password'
+                        name='password_confirmation' 
+                        placeholder='Password confirmation' 
+                        value={this.state.password_confirmation} 
+                        onChange={this.handleChange}
+                        required />
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Registration;
